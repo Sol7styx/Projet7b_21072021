a91c8033f091071ca9e00d2927e97b7701f1685f
Sol7Styx <sol7styx@gmail.com>
2021-08-05 10:41:33 +0200

    components, router, views

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,5 +1,16 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import Vue from 'vue'
+import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Signup from '../views/Signup.vue'
+import OnePublication from '../views/OnePublication.vue'
+import NewPublication from '../views/NewPublication.vue'
+import UserProfil from '../views/UserProfil.vue'
+import OneUserPublications from '../views/OneUserPublications.vue'
+import PublicationsMostRecent from '../views/PublicationsMostRecent.vue'
+import PublicationsMostLiked from '../views/PublicationsMostLiked.vue'
+import PublicationsMostCommented from '../views/PublicationsMostCommented.vue'
+
+Vue.use(VueRouter)
 
 const routes = [
   {
@@ -8,18 +19,51 @@ const routes = [
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/signup',
+    name: 'Signup',
+    component: Signup
+  },
+  {
+    path: '/publication/:id',
+    name: 'OnePublication',
+    component: OnePublication
+  },
+  {
+    path: '/publications/user',
+    name: 'OneUserPublications',
+    component: OneUserPublications
+  },
+  {
+    path: '/user/profil',
+    name: 'UserProfil',
+    component: UserProfil
+  },
+  {
+    path: '/newpublication',
+    name: 'NewPublication',
+    component: NewPublication
+  },
+  {
+    path: '/publications/most-recent',
+    name: 'PublicationsMostRecent',
+    component: PublicationsMostRecent
+  },
+  {
+    path: '/publications/most-liked',
+    name: 'PublicationsMostLiked',
+    component: PublicationsMostLiked
+  },
+  {
+    path: '/publications/most-commented',
+    name: 'PublicationsMostCommented',
+    component: PublicationsMostCommented
   }
 ]
 
-const router = createRouter({
-  history: createWebHashHistory(),
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
   routes
 })
 
-export default router
+export default router;
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,18 +1,46 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Login v-if="!approuvedConnexion"/>
+    <Header v-if="approuvedConnexion"/>
+    <UserNav v-if="approuvedConnexion"/>
+    <PublicationsNav v-if="approuvedConnexion"/>
+    <Publications v-if="approuvedConnexion"/>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import Login from '@/components/Login.vue';
+import Header from '@/components/Header.vue';
+import UserNav from '@/components/UserNav.vue';
+import PublicationsNav from '@/components/PublicationsNav.vue';
+import Publications from '@/components/Publications.vue';
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
+  components: {     // déclaration des composants utilisés par la Vue
+    Login,
+    Header,
+    UserNav,
+    PublicationsNav,
+    Publications
+  },
+  data() {
+    return{
+      approuvedConnexion: false     // on déclare une varibale de type boléen, false par défault (contiendra la validation comme quoi un utilisateur est authentifié)
+    };
+  },
+  created(){                        // hook de cycle de vie qui intervient avant le hook mounted et vérifie la session utilisateur (Item dans le localStorage)
+    this.connectedUser()
+  },
+  methods: {
+    connectedUser(){                // fonction de vérification de la session utilisateur (Item dans le localStorage)
+      if(localStorage.groupomaniaUser == undefined){
+        this.approuvedConnexion = false;
+        console.log('Utilisateur non connecté !');
+      } else {
+        this.approuvedConnexion = true;
+        console.log('Utilisateur connecté !');
+      }
+    }
   }
 }
 </script>
